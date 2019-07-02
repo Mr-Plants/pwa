742d9988c53af4738b0da447e7153728eeda9e6b
weiyb <weiyb@jiedaibao.com>
2019-07-02 20:35:57 +0800

    add sw

--- sw.js
+++ sw.js
@@ -1,6 +1,24 @@
+let version = '1.0';
+let cacheFiles = [
+    // 'index.html'
+];
+
 // 安装回调
 self.addEventListener('install', ev => {
-    console.log(ev)
+    ev.waitUntil(
+        // 创建一个缓存版本，返回promise
+        caches.open(version).then(
+            cache => {
+                console.log('install success');
+                // resolve时缓存cacheFiles所有文件
+                return cache.addAll(cacheFiles);
+            },
+            err => {
+                console.log('install failed');
+            }
+        )
+    )
+    console.log('已安装')
 })
 
 self.addEventListener('activate', ev => {
@@ -8,5 +26,13 @@ self.addEventListener('activate', ev => {
 })
 
 self.addEventListener('fetch', (ev) => {
+    ev.respondWith(
+        caches.match(ev.request).then(
+            res => {
+                console.log(res);
+                return res || fetch(ev.request);
 
+            }
+        )
+    )
 })
